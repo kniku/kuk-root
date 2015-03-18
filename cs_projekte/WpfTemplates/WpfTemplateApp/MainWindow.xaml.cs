0e8bf8bd18a22981e26874b4125708d8652ef699
Kurt Kniely <kniku@users.noreply.github.com>
2015-03-18 20:44:42 +0100

    WpfTemplate added

---
+++ cs_projekte/WpfTemplates/WpfTemplateApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace WpfTemplateApp
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void mcClose(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+		private void mcAbout(object sender, RoutedEventArgs e)
+		{
+			this.ShowMessageAsync("WpfTemplateApp", "Einfaches Anwendungsgerüst einer Wpf Applikation im Metro-Stil...");
+		}
+	}
+}
