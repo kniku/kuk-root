cbc8515fe6eca9bd91f5771bd24d0249655b7845
kniku <kurt.kniely@gmail.com>
2016-06-28 14:10:26 +0200

    javascript example...

---
+++ cs_projekte/einfacherHttpServer/einfacherHttpServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace einfacherHttpServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SimpleHTTPServer s = new SimpleHTTPServer(@"c:\knk\tmp\htdocs", 12000);
+			System.Console.WriteLine("server started on port " + s.Port);
+		}
+	}
+}
