9d036d110255fe203a5b3fb0da58553e314fe2c4
Kurt Kniely <kurt.kniely@automationx.com>
2022-07-29 18:24:12 +0200

    ...

--- PowerShells/Template.ps1
+++ PowerShells/Template.ps1
@@ -2,9 +2,16 @@
 .Description
 This is a template ps1 script.
 #>
+[CmdletBinding(DefaultParameterSetName = 'seta')] # default parameter set if mutually exclusive parameters used
 Param (
     # Description of: help...
-	[switch]$help = $false
+	[switch]
+	[parameter(ParameterSetName="seta",position=0)] # mutually exclusive parameters
+	$help = $false,
+    # Description of: help2...
+	[switch]
+	[parameter(ParameterSetName="setb",position=0)] # mutually exclusive parameters
+	$help2 = $true
 )
 
 if ($PSBoundParameters.Count -eq 0) {
